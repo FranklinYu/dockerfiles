e110544083bbfe7d72254396ae4b4cf08986341f
Franklin Yu <franklinyu@hotmail.com>
2019-08-07 11:02:56 -0400

    Remove Ruby version
    I'm not using it anyway. Ruby in Alpine is used instead. It's too
    expensive to track Ruby version in personal project.

--- sinatra-hello/gems.rb
+++ sinatra-hello/gems.rb
@@ -1,11 +1,4 @@
 source 'https://rubygems.org'
 
-begin
-  ruby File.read('.ruby-version').strip
-rescue Errno::ENOENT
-  # Ruby version file is missing.
-end
-
 gem 'sinatra', '~> 1.4'
-
 gem 'puma', group: :production
